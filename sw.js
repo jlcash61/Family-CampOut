5a7c5621edc91381606ef04d699b0fd60d8da5a4
Jeff Cash <tiborg.iot@gmail.com>
2025-05-12 22:51:36 -0400

    v10

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE = 'familycampout-v9';
+const CACHE = 'familycampout-v10';
 const ASSETS = [
   '/',                 // clean URL → index.html
-  'index.html',
-  'map.html',
-  'rules.html',
-  'waterzone.html',
-  'style.css',
-  'icons/icon-512.png',
-  'images/jelleyStoneMap.png',
-  'images/jelleyStoneMapHD.png'
+  '/index.html',
+  '/map.html',
+  '/rules.html',
+  '/waterzone.html',
+  '/style.css',
+  '/icons/icon-512.png',
+  '/images/jelleyStoneMap.png',
+  '/images/jelleyStoneMapHD.png'
 ];
 
 self.addEventListener('install', event => {
